02771e5a287be89c14ddb175eb9b6e924c70b010
aloaloolaola <alo.virunurm@gmail.com>
2023-10-10 12:19:26 +0300

    first commit

---
+++ TEE.py
@@ -0,0 +1,5 @@
+soojusmahtuvus, kuumutuskiirus, soojashoiuenergia, jahtumiskiirus = [float(a) for a in input().split(" ")]
+print(soojusmahtuvus, kuumutuskiirus, soojashoiuenergia, jahtumiskiirus)
+
+soojashoiuenergiahulk = 60 * soojashoiuenergia
+print(soojashoiuenergiahulk)
