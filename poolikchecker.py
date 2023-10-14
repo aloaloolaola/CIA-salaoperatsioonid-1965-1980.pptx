07f791aeaf18d9a578300f431d5c61cc9b685908
aloaloolaola <alo.virunurm@gmail.com>
2023-10-15 00:20:37 +0300

    99.97412p

--- poolikchecker.py
+++ poolikchecker.py
@@ -1,4 +1,4 @@
-VOWELS = "aeiouõäöüáéíóúyæ"
+VOWELS = "aeiouõäöüáéíóúæ"
 
 def remove_vowels(string: str):
     return "".join(char for char in string if not char.lower() in VOWELS)
@@ -17,11 +17,11 @@ def check_file(broken: list[str], original: list[str]):
     for i in range(len(broken)):
         if not broken[i] == broken1[i]:
             errors += 1
-            print(i, broken[i], broken1[i])
+            print(i, "\n", broken[i], "\n", broken1[i], "\n")
     return errors
 
-file0 = open("testid/input_002.txt", "r", encoding="cp850")
-file1 = open("testid/output_002.txt", "r", encoding="cp850")
+file0 = open("testid/input_004.txt", "r", encoding="utf-8")
+file1 = open("testid/output_004.txt", "r", encoding="utf-8")
 content0 = [row for row in file0]
 content1 = [row for row in file1]
 file0.close()
